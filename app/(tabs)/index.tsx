64c9d5dc4d66f20e607c838e8dc6f36323e193ff
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-07 02:10:10 +0800

    qweqwe

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,34 +2,12 @@ import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import StoryList from "../../components/stories/storylist";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text className="flex">Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="flex">
+      <StoryList />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
---
+++ components/stories/storylist/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StoryList = () => {
+  return (
+    <View>
+      <Text>StoryList</Text>
+    </View>
+  );
+};
+
+export default StoryList;
+
+const styles = StyleSheet.create({});
