7e2c07a8369c890513f652eea8a5b44076437799
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-08 01:18:38 +0800

    wde

--- components/slider/sliderlist/index.tsx
+++ components/slider/sliderlist/index.tsx
@@ -1,45 +1,21 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import SliderSingle from "../slidersingle";
+import { Image, View, Dimensions } from "react-native";
 
 const SliderList = () => {
-  const items = [
-    {
-      id: 1,
-      image: "https://img.youtube.com/vi/eBMxIV-0rvE/mqdefault.jpg",
-      title:
-        "How did British Empire take over India? What about the other countries",
-      description:
-        "This is a great guide to creating a new Indian film film series that will be made available to consumers everywhere in the world and will be made available to consumers in the world of Indian film production and film production that",
-    },
-    {
-      id: 2,
-      image: "https://img.youtube.com/vi/eBMxIV-0rvE/mqdefault.jpg",
-      title:
-        "How did British Empire take over India? What about the other countries",
-      description:
-        "This is a great guide to creating a new Indian film film series that will be made available to consumers everywhere in the world and will be made available to consumers in the world of Indian film production and film production that",
-    },
-    {
-      id: 3,
-      image: "https://img.youtube.com/vi/eBMxIV-0rvE/mqdefault.jpg",
-      title:
-        "How did British Empire take over India? What about the other countries",
-      description:
-        "This is a great guide to creating a new Indian film film series that will be made available to consumers everywhere in the world and will be made available to consumers in the world of Indian film production and film production that",
-    },
-  ];
+  const image: any =
+    "https://dev-resws-hungamatech-com.storage.googleapis.com/featured_content/a7219b927cb21e46c5a75856f0e5aca0_924x520.jpg";
+
+  const screenWidth = Dimensions.get("window").width;
+  const bannerWidth = screenWidth - 24; // Adjust the margin/padding as needed
+
   return (
-    <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
-      {items.map((item) => (
-        <SliderSingle
-          key={item.id}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-    </ScrollView>
+    <View className="p-4 flex items-center">
+      <Image
+        source={{ uri: image }}
+        style={{ width: bannerWidth, height: 130 }}
+        className="rounded-lg"
+      />
+    </View>
   );
 };
 
