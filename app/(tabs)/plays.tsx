a56ca83f89d5a3c4f173b42e45bb72341a146d4d
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-16 02:03:49 +0800

    dwd

--- app/(tabs)/plays.tsx
+++ app/(tabs)/plays.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "../../components/Themed";
+import { View, Dimensions } from "react-native";
 import PlaysVO from "../../components/playssongs/plays";
 
 export default function Plays() {
   const backgroundColor = "#222222";
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
   return (
-    <View style={{ backgroundColor: backgroundColor }} className="flex">
+    <View
+      style={{ backgroundColor: backgroundColor, width, height }}
+      className="flex"
+    >
       <PlaysVO />
     </View>
   );
--- components/playssongs/plays/index.tsx
+++ components/playssongs/plays/index.tsx
@@ -1,117 +1,187 @@
-import { Text, View, Image, Dimensions, TouchableOpacity } from "react-native";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+
 import { ResizeMode, Video } from "expo-av";
-import { Ionicons } from "@expo/vector-icons";
+import { Entypo, AntDesign, FontAwesome, Fontisto } from "@expo/vector-icons";
 
-const PlaysVO = () => {
-  const width = Dimensions.get("window").width;
-  const height = Dimensions.get("window").height;
-  const backgroundColor = "#222222";
-  const playbackInstance = useRef(null);
+const Post = () => {
+  const [post, setPost] = useState(null);
+  const [isLiked, setIsLiked] = useState(false);
   const videook: any = require("../../../assets/video/vidok.mp4");
+  const playbackInstance = useRef(null);
 
-  const handleLikePress = () => {
-    // Handle like functionality
+  const [paused, setPaused] = useState(false);
+
+  const onPlayPausePress = () => {
+    setPaused(!paused);
   };
 
-  const handleCommentPress = () => {
-    // Handle comment functionality
-  };
+  const onLikePress = () => {};
 
-  const handleSharePress = () => {
-    // Handle share functionality
-  };
+  const getVideoUri = async () => {};
+
+  useEffect(() => {
+    getVideoUri();
+  }, []);
 
   return (
-    <View style={{ backgroundColor: backgroundColor, height, width }}>
-      {/* Video Player */}
-      <Video
-        ref={playbackInstance}
-        source={videook}
-        style={{ width: width }}
-        className="h-48 my-2"
-        resizeMode={ResizeMode.CONTAIN}
-        shouldPlay
-      />
-      <View className="absolute bottom-20 left-20">
-        {/* Like Icon */}
-        <TouchableOpacity onPress={handleLikePress}>
-          <Ionicons name="heart-outline" size={24} color="white" />
-        </TouchableOpacity>
-        {/* Comment Icon */}
-        <TouchableOpacity onPress={handleCommentPress}>
-          <Ionicons name="chatbubble-outline" size={24} color="white" />
-        </TouchableOpacity>
-
-        {/* Share Icon */}
-        <TouchableOpacity onPress={handleSharePress}>
-          <Ionicons name="share-social-outline" size={24} color="white" />
-        </TouchableOpacity>
-
-        {/* User Information */}
-        <View
-          style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}
-        >
-          <Image
-            source={require("../../../assets/images/favicon.png")}
-            style={{ width: 30, height: 30, borderRadius: 15, marginRight: 10 }}
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={onPlayPausePress}>
+        <View>
+          <Video
+            ref={playbackInstance}
+            source={videook}
+            style={styles.video}
+            className="h-48 my-2"
+            resizeMode={ResizeMode.CONTAIN}
+            shouldPlay
           />
-          <Text style={{ color: "white", fontWeight: "bold" }}>Username</Text>
-        </View>
-        {/* Music and Description */}
-        <View style={{ marginTop: 10 }}>
-          <Text style={{ color: "white" }}>Song Name - Artist</Text>
-          <Text style={{ color: "white" }}>Description</Text>
-        </View>
-        {/* Additional UI Elements */}
-        <View
-          style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}
-        >
-          {/* Followers Count */}
-          <Ionicons name="people-outline" size={18} color="white" />
-          <Text style={{ color: "white", marginLeft: 5 }}>1.2M Followers</Text>
 
-          {/* Views Count */}
-          <Ionicons
-            name="eye-outline"
-            size={18}
-            color="white"
-            style={{ marginLeft: 20 }}
-          />
-          <Text style={{ color: "white", marginLeft: 5 }}>1.5M Views</Text>
-          {/* Actions */}
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 10,
-            }}
-          >
-            {/* Follow Button */}
-            <TouchableOpacity
-              style={{
-                backgroundColor: "#FF0000",
-                paddingVertical: 5,
-                paddingHorizontal: 10,
-                borderRadius: 10,
-              }}
-            >
-              <Text style={{ color: "white" }}>Follow</Text>
-            </TouchableOpacity>
-
-            {/* More Options */}
-            <TouchableOpacity style={{ marginLeft: 10 }}>
-              <Ionicons
-                name="ellipsis-horizontal-outline"
-                size={24}
-                color="white"
+          <View style={styles.uiContainer}>
+            <View style={styles.rightContainer}>
+              <Image
+                style={styles.profilePicture}
+                source={require("../../../assets/images/favicon.png")}
               />
-            </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.iconContainer}
+                onPress={onLikePress}
+              >
+                <AntDesign
+                  name={"heart"}
+                  size={25}
+                  color={isLiked ? "red" : "white"}
+                />
+                <Text style={styles.statsLabel}>222</Text>
+              </TouchableOpacity>
+
+              <View style={styles.iconContainer}>
+                <FontAwesome name={"commenting"} size={25} color="white" />
+                <Text style={styles.statsLabel}>2734</Text>
+              </View>
+
+              <View style={styles.iconContainer}>
+                <Fontisto name={"share-a"} size={25} color="white" />
+                <Text style={styles.statsLabel}>424</Text>
+              </View>
+            </View>
+
+            <View style={styles.bottomContainer}>
+              <View>
+                <Text style={styles.handle}>@jaydeepdhrangiya</Text>
+                <Text style={styles.description}>Song Name - Artist</Text>
+
+                <View style={styles.songRow}>
+                  <Entypo name={"beamed-note"} size={24} color="white" />
+                  <Text style={styles.songName}>Song Name - Artist</Text>
+                </View>
+              </View>
+
+              <Image
+                style={styles.songImage}
+                source={require("../../../assets/images/favicon.png")}
+              />
+            </View>
           </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
 
-export default PlaysVO;
+export default Post;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: Dimensions.get("window").height - 130,
+  },
+  videPlayButton: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: 100,
+  },
+  video: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  uiContainer: {
+    height: "100%",
+    justifyContent: "flex-end",
+  },
+  bottomContainer: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  handle: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 10,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+  songRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  songName: {
+    color: "#fff",
+    fontSize: 16,
+    marginLeft: 5,
+  },
+
+  songImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 5,
+    borderColor: "#4c4c4c",
+  },
+
+  //  right container
+  rightContainer: {
+    alignSelf: "flex-end",
+    height: 300,
+    justifyContent: "space-between",
+    marginRight: 5,
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+
+  iconContainer: {
+    alignItems: "center",
+  },
+  statsLabel: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+    marginTop: 5,
+  },
+});
