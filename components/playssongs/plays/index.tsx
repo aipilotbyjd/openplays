e76784789fc29a90875c961249118d1fe4d49a34
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-16 01:14:07 +0800

    qs

--- components/playssongs/plays/index.tsx
+++ components/playssongs/plays/index.tsx
@@ -23,7 +23,7 @@ const PlaysVO = () => {
   };
 
   return (
-    <View style={{ backgroundColor: backgroundColor }}>
+    <View style={{ backgroundColor: backgroundColor, height, width }}>
       {/* Video Player */}
       <Video
         ref={playbackInstance}
@@ -33,6 +33,83 @@ const PlaysVO = () => {
         resizeMode={ResizeMode.CONTAIN}
         shouldPlay
       />
+      <View className="absolute bottom-20 left-20">
+        {/* Like Icon */}
+        <TouchableOpacity onPress={handleLikePress}>
+          <Ionicons name="heart-outline" size={24} color="white" />
+        </TouchableOpacity>
+        {/* Comment Icon */}
+        <TouchableOpacity onPress={handleCommentPress}>
+          <Ionicons name="chatbubble-outline" size={24} color="white" />
+        </TouchableOpacity>
+
+        {/* Share Icon */}
+        <TouchableOpacity onPress={handleSharePress}>
+          <Ionicons name="share-social-outline" size={24} color="white" />
+        </TouchableOpacity>
+
+        {/* User Information */}
+        <View
+          style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}
+        >
+          <Image
+            source={require("../../../assets/images/favicon.png")}
+            style={{ width: 30, height: 30, borderRadius: 15, marginRight: 10 }}
+          />
+          <Text style={{ color: "white", fontWeight: "bold" }}>Username</Text>
+        </View>
+        {/* Music and Description */}
+        <View style={{ marginTop: 10 }}>
+          <Text style={{ color: "white" }}>Song Name - Artist</Text>
+          <Text style={{ color: "white" }}>Description</Text>
+        </View>
+        {/* Additional UI Elements */}
+        <View
+          style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}
+        >
+          {/* Followers Count */}
+          <Ionicons name="people-outline" size={18} color="white" />
+          <Text style={{ color: "white", marginLeft: 5 }}>1.2M Followers</Text>
+
+          {/* Views Count */}
+          <Ionicons
+            name="eye-outline"
+            size={18}
+            color="white"
+            style={{ marginLeft: 20 }}
+          />
+          <Text style={{ color: "white", marginLeft: 5 }}>1.5M Views</Text>
+          {/* Actions */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 10,
+            }}
+          >
+            {/* Follow Button */}
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#FF0000",
+                paddingVertical: 5,
+                paddingHorizontal: 10,
+                borderRadius: 10,
+              }}
+            >
+              <Text style={{ color: "white" }}>Follow</Text>
+            </TouchableOpacity>
+
+            {/* More Options */}
+            <TouchableOpacity style={{ marginLeft: 10 }}>
+              <Ionicons
+                name="ellipsis-horizontal-outline"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
