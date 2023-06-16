34e34d09a6d1e140f11069b007c2cb89688c6b1a
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-16 21:25:35 +0800

    sdqsn

--- app/(tabs)/plays.tsx
+++ app/(tabs)/plays.tsx
@@ -2,7 +2,7 @@ import { View, Dimensions } from "react-native";
 import PlaysVO from "../../components/playssongs/plays";
 
 export default function Plays() {
-  const backgroundColor = "#222222";
+  const backgroundColor = "#806A56";
   const width = Dimensions.get("window").width;
   const height = Dimensions.get("window").height;
   return (
