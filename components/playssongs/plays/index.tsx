f641ee92a9c092398337ec6d9e1c6754bad1e69d
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-17 15:18:14 +0800

    wdd

--- components/playssongs/plays/index.tsx
+++ components/playssongs/plays/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,148 +7,74 @@ import {
   Dimensions,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Ionicons, AntDesign, FontAwesome } from "@expo/vector-icons";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const MusicPlayer = () => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const handleToggleFollow = () => {
+    setIsFollowing((prevState) => !prevState);
+  };
+
   const windowWidth = Dimensions.get("window").width;
   const windowHeight = Dimensions.get("window").height;
-
-  const everyviewheight = windowHeight / 3;
+  const fixHeight = windowHeight / 3;
 
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: everyviewheight,
-          backgroundColor: "#67B26F",
-          paddingHorizontal: windowWidth * 0.02,
-          paddingTop: 20,
-          borderBottomRightRadius: 30,
-          borderBottomLeftRadius: 30,
-          zIndex: 2,
-          elevation: 10,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <TouchableOpacity style={{ marginLeft: 10 }}>
-          <FontAwesome
-            name="chevron-left"
-            size={windowWidth * 0.05}
-            color="white"
-          />
-        </TouchableOpacity>
-        <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>
-          Comrade Anthem (From "Dear Comrade")
-        </Text>
-        <TouchableOpacity style={{ marginRight: 10 }}>
-          <Ionicons
-            name="ellipsis-vertical"
-            size={windowWidth * 0.05}
-            color="white"
-          />
-        </TouchableOpacity>
+    <View className="flex flex-col">
+      <View className="w-full h-[33.333333%]">
+        <LinearGradient
+          colors={["#3CA55C", "#B5AC49"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          className="absolute flex-row items-center px-4"
+          style={{ width: windowWidth, height: fixHeight }}
+        >
+          <TouchableOpacity>
+            <FontAwesome5 name="chevron-left" size={24} color="white" />
+          </TouchableOpacity>
+          <Text className="text-white text-xs ml-2">
+            Comrade Anthem (From "Dear Comrade")
+          </Text>
+        </LinearGradient>
       </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: 20,
-          height: everyviewheight,
-        }}
-      >
+      <View className="w-full h-[33.333333%]">
         <ImageBackground
           source={require("../../../assets/images/song.jpg")}
           resizeMode="cover"
-          style={{
-            width: windowWidth - 40,
-            height: "100%",
-            borderRadius: 20,
-            justifyContent: "flex-end",
-            alignItems: "center",
-            overflow: "hidden",
-            shadowColor: "black",
-            shadowOffset: { width: 0, height: 5 },
-            shadowOpacity: 0.3,
-            shadowRadius: 10,
-            elevation: 10,
-          }}
-        >
-          <View style={{ backgroundColor: "rgba(0, 0, 0, 0.4)", padding: 10 }}>
-            <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
-              Song Title
-            </Text>
-            <Text style={{ color: "white", fontSize: 16 }}>Artist Name</Text>
-          </View>
-        </ImageBackground>
+          style={{ width: windowWidth, height: fixHeight }}
+        />
       </View>
-      <LinearGradient
-        colors={["#3CA55C", "#B5AC49"]}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-        style={{
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          width: "100%",
-          height: everyviewheight,
-          paddingTop: 20,
-          borderTopLeftRadius: 30,
-          borderTopRightRadius: 30,
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontSize: 24,
-            fontWeight: "bold",
-            marginBottom: 20,
-            textAlign: "center",
-          }}
+      <View className="w-full h-[33.333333%] bottom-10">
+        <LinearGradient
+          colors={["#3CA55C", "#B5AC49"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          className="absolute flex-row items-center px-4"
+          style={{ width: windowWidth, height: fixHeight }}
         >
-          Comrade Anthem
-        </Text>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-around",
-            alignItems: "center",
-            paddingHorizontal: 20,
-          }}
-        >
-          <TouchableOpacity>
-            <AntDesign name="hearto" size={windowWidth * 0.06} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome
-              name="comment-o"
-              size={windowWidth * 0.06}
-              color="white"
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <AntDesign
-              name="sharealt"
-              size={windowWidth * 0.06}
-              color="white"
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <AntDesign
-              name="download"
-              size={windowWidth * 0.06}
-              color="white"
-            />
-          </TouchableOpacity>
-        </View>
-      </LinearGradient>
+          <View className="flex-1">
+            <View className="flex">
+              <Text className="text-white text-lg font-bold">
+                Comrade Anthem
+              </Text>
+            </View>
+            <View className="flex flex-row space-x-2 items-center">
+              <Text className="text-white text-xs">Rajbha Gadhavi</Text>
+              <TouchableOpacity
+                onPress={handleToggleFollow}
+                className="flex border border-white rounded-3xl"
+              >
+                <Text
+                  className={`text-white text-[10px] px-2 font-bold text-center`}
+                >
+                  {isFollowing ? "Following" : "Follow"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
