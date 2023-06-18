7a46d2414fcea7b9a3c5452861a3287f440a180f
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-18 15:05:46 +0800

    sq

--- app/(tabs)/plays.tsx
+++ app/(tabs)/plays.tsx
@@ -1,16 +1,84 @@
-import { View, Dimensions } from "react-native";
+import React, { useRef } from "react";
+import { View, Dimensions, FlatList } from "react-native";
 import PlaysVO from "../../components/playssongs/plays";
 
-export default function Plays() {
-  const backgroundColor = "#806A56";
-  const width = Dimensions.get("window").width;
-  const height = Dimensions.get("window").height;
+const backgroundColor = "#806A56";
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+interface Song {
+  id: string;
+  name: string;
+  artist: string;
+  duration: string;
+  genre: string;
+}
+
+const Plays = () => {
+  const flatListRef = useRef<FlatList<Song>>(null);
+
+  const songs: Song[] = [
+    {
+      id: "1",
+      name: "Dear Comrade",
+      artist: "Rajbha Gadhavi",
+      duration: "3:45",
+      genre: "Pop",
+      // Add more properties as needed
+    },
+    {
+      id: "2",
+      name: "Song 2",
+      artist: "Artist 2",
+      duration: "4:20",
+      genre: "Rock",
+      // Add more properties as needed
+    },
+    {
+      id: "3",
+      name: "Song 3",
+      artist: "Artist 3",
+      duration: "2:55",
+      genre: "Hip Hop",
+      // Add more properties as needed
+    },
+    {
+      id: "4",
+      name: "Song 4",
+      artist: "Artist 4",
+      duration: "3:30",
+      genre: "Electronic",
+      // Add more properties as needed
+    },
+    {
+      id: "5",
+      name: "Song 5",
+      artist: "Artist 5",
+      duration: "5:10",
+      genre: "R&B",
+      // Add more properties as needed
+    },
+    // Add more song objects as needed
+  ];
+
+  const renderItem = ({ item }: { item: Song }) => {
+    return <PlaysVO item={item} />;
+  };
+
   return (
-    <View
-      style={{ backgroundColor: backgroundColor, width, height }}
-      className="flex"
-    >
-      <PlaysVO />
+    <View style={{ backgroundColor }}>
+      <FlatList
+        ref={flatListRef}
+        data={songs}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        snapToInterval={height}
+        decelerationRate="fast"
+        pagingEnabled={true}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
-}
+};
+
+export default Plays;
--- components/playssongs/plays/index.tsx
+++ components/playssongs/plays/index.tsx
@@ -18,13 +18,33 @@ import {
 } from "@expo/vector-icons";
 import Slider from "@react-native-community/slider";
 
-const MusicPlayer = () => {
+interface Song {
+  id: string;
+  name: string;
+  artist: string;
+  duration: string;
+  genre: string;
+}
+
+interface PlaysVOProps {
+  item: Song;
+}
+
+const MusicPlayer = (props: PlaysVOProps) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isFollowing, setIsFollowing] = useState(false);
   const [isRepeat, setIsRepeat] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [currentLineIndex, setCurrentLineIndex] = useState(0);
   const opacityValue = new Animated.Value(0);
+  const [post, setPost] = useState<PlaysVOProps | null>(null);
+
+  useEffect(() => {
+    if (props) {
+      setPost(() => props);
+      console.log(post);
+    }
+  }, [props]);
 
   const textLines = [
     "Hey girl, are you all right",
@@ -79,150 +99,156 @@ const MusicPlayer = () => {
   const fixHeight = windowHeight / 3;
 
   return (
-    <View className="flex pb-56">
-      <View className="w-full h-[30%]">
-        <LinearGradient
-          colors={["#3CA55C", "#B5AC49", "#6CCE8D"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 1 }}
-          locations={[0.1, 0.9, 0.1]}
-          className="absolute"
-          style={{ width: windowWidth, height: fixHeight }}
-        >
-          <View className="flex flex-row items-center justify-center py-2">
-            <Text
-              className="text-white text-xs ml-2"
-              style={{ fontFamily: "LilitaOne" }}
-            >
-              Next.js Full Course for Beginners | Nextjs 13 Tutorial | 7 Hours
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-center py-2">
-            {textLines.map((line, index) => (
-              <Animated.View
-                key={index}
-                style={{
-                  opacity: opacityValue,
-                  marginBottom: 8,
-                }}
-              >
-                <Text
-                  style={{
-                    color: "white",
-                    fontSize: 14,
-                    fontFamily: "LilitaOne",
-                  }}
-                >
-                  {index === currentLineIndex ? line : ""}
-                </Text>
-              </Animated.View>
-            ))}
-          </View>
-        </LinearGradient>
-      </View>
-      <View className="w-full h-[50%]">
-        <ImageBackground
-          source={require("../../../assets/images/song.jpg")}
-          resizeMode="cover"
-          style={{ width: "100%" }}
-          className="h-[120%] pt-10"
-        />
-      </View>
-      <View className="w-full h-[20%]">
-        <LinearGradient
-          colors={["#3CA55C", "#B5AC49", "#46FFF9"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 1 }}
-          locations={[0, 1, 0]}
-          className="absolute flex-row items-center px-4 top-10 pb-24"
-          style={{ width: windowWidth, height: fixHeight }}
-        >
-          <View className="flex-1">
-            <View className="flex">
+    <View style={{ width: windowWidth, height: windowHeight }} className="flex">
+      <View className="flex pb-56">
+        <View className="w-full h-[30%]">
+          <LinearGradient
+            colors={["#3CA55C", "#B5AC49", "#6CCE8D"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            locations={[0.1, 0.9, 0.1]}
+            className="absolute"
+            style={{ width: windowWidth, height: fixHeight }}
+          >
+            <View className="flex flex-row items-center justify-center py-2">
               <Text
-                className="text-white text-lg"
+                className="text-white text-xs ml-2"
                 style={{ fontFamily: "LilitaOne" }}
               >
-                Comrade Anthem
+                Next.js Full Course for Beginners | Nextjs 13 Tutorial | 7 Hours
               </Text>
             </View>
-            <View className="flex flex-row space-x-2 items-center py-2">
-              <Text
-                className="text-white text-xs"
-                style={{ fontFamily: "LilitaOne" }}
-              >
-                Rajbha Gadhavi
-              </Text>
-              <TouchableOpacity
-                onPress={handleToggleFollow}
-                className="flex border border-white rounded-3xl"
-              >
+
+            <View className="flex flex-row items-center justify-center py-2">
+              {textLines.map((line, index) => (
+                <Animated.View
+                  key={index}
+                  style={{
+                    opacity: opacityValue,
+                    marginBottom: 8,
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontSize: 14,
+                      fontFamily: "LilitaOne",
+                    }}
+                  >
+                    {index === currentLineIndex ? line : ""}
+                  </Text>
+                </Animated.View>
+              ))}
+            </View>
+          </LinearGradient>
+        </View>
+        <View className="w-full h-[50%]">
+          <ImageBackground
+            source={require("../../../assets/images/song.jpg")}
+            resizeMode="cover"
+            style={{ width: "100%" }}
+            className="h-[120%] pt-10"
+          />
+        </View>
+        <View className="w-full h-[20%]">
+          <LinearGradient
+            colors={["#3CA55C", "#B5AC49", "#46FFF9"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            locations={[0, 1, 0]}
+            className="absolute flex-row items-center px-4 top-10 pb-24"
+            style={{ width: windowWidth, height: fixHeight }}
+          >
+            <View className="flex-1">
+              <View className="flex">
                 <Text
-                  className="text-white text-[10px] px-2 text-center"
+                  className="text-white text-lg"
                   style={{ fontFamily: "LilitaOne" }}
                 >
-                  {isFollowing ? "Following" : "Follow"}
+                  {post ? post.item.name : "unknown name"}
                 </Text>
-              </TouchableOpacity>
-            </View>
-            <View className="flex flex-row justify-between items-center">
-              <View className="flex flex-row space-x-8 items-center">
-                <TouchableOpacity onPress={handleToggleLike}>
+              </View>
+              <View className="flex flex-row space-x-2 items-center py-2">
+                <Text
+                  className="text-white text-xs"
+                  style={{ fontFamily: "LilitaOne" }}
+                >
+                  {post ? post.item.artist : "unknown artist"}
+                </Text>
+                <TouchableOpacity
+                  onPress={handleToggleFollow}
+                  className="flex border border-white rounded-3xl"
+                >
+                  <Text
+                    className="text-white text-[10px] px-2 text-center"
+                    style={{ fontFamily: "LilitaOne" }}
+                  >
+                    {isFollowing ? "Following" : "Follow"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View className="flex flex-row justify-between items-center">
+                <View className="flex flex-row space-x-8 items-center">
+                  <TouchableOpacity onPress={handleToggleLike}>
+                    <Ionicons
+                      name="md-heart"
+                      size={24}
+                      color={isLiked ? "red" : "white"}
+                    />
+                  </TouchableOpacity>
+                  <Octicons name="comment" size={24} color="white" />
                   <Ionicons
-                    name="md-heart"
+                    name="ios-share-social-sharp"
                     size={24}
-                    color={isLiked ? "red" : "white"}
+                    color="white"
                   />
-                </TouchableOpacity>
-                <Octicons name="comment" size={24} color="white" />
-                <Ionicons
-                  name="ios-share-social-sharp"
-                  size={24}
-                  color="white"
-                />
+                </View>
+                <View className="flex flex-row space-x-8 items-center">
+                  <MaterialCommunityIcons
+                    name="cloud-download-outline"
+                    size={24}
+                    color="white"
+                  />
+                  <TouchableOpacity onPress={handleToggleRepeat}>
+                    {isRepeat ? (
+                      <MaterialIcons
+                        name="repeat-one"
+                        size={24}
+                        color="white"
+                      />
+                    ) : (
+                      <MaterialIcons name="repeat" size={24} color="white" />
+                    )}
+                  </TouchableOpacity>
+                  <Entypo name="dots-three-vertical" size={24} color="white" />
+                </View>
               </View>
-              <View className="flex flex-row space-x-8 items-center">
-                <MaterialCommunityIcons
-                  name="cloud-download-outline"
-                  size={24}
-                  color="white"
-                />
-                <TouchableOpacity onPress={handleToggleRepeat}>
-                  {isRepeat ? (
-                    <MaterialIcons name="repeat-one" size={24} color="white" />
-                  ) : (
-                    <MaterialIcons name="repeat" size={24} color="white" />
-                  )}
-                </TouchableOpacity>
-                <Entypo name="dots-three-vertical" size={24} color="white" />
-              </View>
-            </View>
-            <View className="flex flex-row items-center px-1 mb-[-60]">
-              <View className="flex w-1/10">
-                <TouchableOpacity onPress={handleTogglePlay}>
-                  {isPlaying ? (
-                    <FontAwesome5 name="pause" size={24} color="white" />
-                  ) : (
-                    <FontAwesome5 name="play" size={24} color="white" />
-                  )}
-                </TouchableOpacity>
-              </View>
-              <View className="flex w-9/10">
-                <Slider
-                  style={{
-                    width: windowWidth * 0.86,
-                    height: windowHeight * 0.06,
-                  }}
-                  minimumValue={0}
-                  maximumValue={1}
-                  minimumTrackTintColor="#fff8dc"
-                  maximumTrackTintColor="#f8f8ff"
-                />
+              <View className="flex flex-row items-center px-1 mb-[-60]">
+                <View className="flex w-1/10">
+                  <TouchableOpacity onPress={handleTogglePlay}>
+                    {isPlaying ? (
+                      <FontAwesome5 name="pause" size={24} color="white" />
+                    ) : (
+                      <FontAwesome5 name="play" size={24} color="white" />
+                    )}
+                  </TouchableOpacity>
+                </View>
+                <View className="flex w-9/10">
+                  <Slider
+                    style={{
+                      width: windowWidth * 0.86,
+                      height: windowHeight * 0.06,
+                    }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    minimumTrackTintColor="#fff8dc"
+                    maximumTrackTintColor="#f8f8ff"
+                  />
+                </View>
               </View>
             </View>
-          </View>
-        </LinearGradient>
+          </LinearGradient>
+        </View>
       </View>
     </View>
   );
